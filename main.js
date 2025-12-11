5c0dd07f65089d2fc1a8ee980dfdafee770d97ef
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-12-11 12:41:13 -0600

    Parse input values as numbers

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const tipPercentInput = document.getElementById("tip-percent");
 const calculateButton = document.getElementById("calculate");
 const outputText = document.getElementById("output");
 calculateButton.addEventListener("click", () => {
-    let amount = amountInput.value;
-    let tipPercent = tipPercentInput.value;
+    let amount = parseFloat(amountInput.value);
+    let tipPercent = parseFloat(tipPercentInput.value);
     let totalAmount = amount * (1 + tipPercent / 100);
     outputText.innerText = `Total Amount: ${totalAmount.toFixed(2)}`;
 });
