a69a598eb41f22ee2d3de4c1ca36a307af6624f8
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-12-11 12:35:27 -0600

    Format total amount to two decimal places

--- main.js
+++ main.js
@@ -6,5 +6,5 @@ calculateButton.addEventListener("click", () => {
     let amount = amountInput.value;
     let tipPercent = tipPercentInput.value;
     let totalAmount = amount * (1 + tipPercent / 100);
-    outputText.innerText = `Total Amount: ${totalAmount}`;
+    outputText.innerText = `Total Amount: ${totalAmount.toFixed(2)}`;
 });
