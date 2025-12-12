e0ede91b93543f32ed0e8ff3211fb92fe788dee0
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-12-12 12:33:25 -0600

    Add even more comments

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const amountInput = document.getElementById("amount"); //amount input
 const tipPercentInput = document.getElementById("tip-percent"); //tip percent input
 const calculateButton = document.getElementById("calculate"); //calculate button
-const outputText = document.getElementById("output"); //output text
+const outputText = document.getElementById("output"); //output text area
 calculateButton.addEventListener("click", () => {
     //when calculate button is pressed
     let amount = parseFloat(amountInput.value); //get amount value
     let tipPercent = parseFloat(tipPercentInput.value); //get tip percent value
     let totalAmount = amount * (1 + tipPercent / 100); //get total billing amount
-    outputText.innerText = `Total Amount: ${totalAmount.toFixed(2)}`; //format total billing amount
+    outputText.innerText = `Total Amount: ${totalAmount.toFixed(2)}`; //set output text area to format total billing amount
 });
