7342df75bd19325c13b51df0a589fbe3fd70bd3e
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-12-12 12:38:55 -0600

    Add more display outputs

--- main.js
+++ main.js
@@ -8,5 +8,10 @@ calculateButton.addEventListener("click", () => {
     let tipPercent = parseFloat(tipPercentInput.value); //get tip percent value
     let totalAmount = amount * (1 + tipPercent / 100); //get total billing amount
     let tipAmount = amount * (tipPercent / 100); //get tip amount
-    outputText.innerText = `Total Amount: ${totalAmount.toFixed(2)}\nTip Amount: ${tipAmount.toFixed(2)}`; //set output text area to format total billing amount and tip amount
+    outputText.innerText = `
+        Total Amount: ${totalAmount.toFixed(2)}
+        Tip Amount: ${tipAmount.toFixed(2)}
+        Tip Percent: ${tipPercent}%
+        Bill Amount: ${amount.toFixed(2)}
+    `; //set output text area to format total billing amount, tip amount, tip percent and bill amount
 });
